f00c97485884aac7a87ddd27fb5cf4fe90a15b7e
Howard Xue <howard_xue@hotmail.com>
2014-01-02 16:41:35 -0800

    Ver 1.0

--- application.js
+++ application.js
@@ -1,21 +1,20 @@
-$document.ready(function() { 
+$(document).ready(function() { 
     var target = Math.floor(Math.random()*99+1); //Global var, generates a number between 1 and 100.
-    function initialize() {
-        $('#submit').click(function() {
-            submit();
-        });
-    };
+    var previousDiff = 0; //previous difference, initially set to 0.
+
     function submit() {
-        var guess = $("#guess_number").val(); //Global var, asks user for a guess.
+        var guess = $("#guess_number").val(); 
         compare(guess);
     }
     function compare(guess) {
-        var number = Number(guess);
-        if (isNaN(number)) {  // if the user input is not a valid number, prompts user for a new guess.
-            $("#response").html("Please enter a number.");
+        var number = Number(guess.trim());
+        if (isNaN(number) || guess=== "" || guess === " " || (number - Math.floor(number) !== 0)) {  // if the user input is not a valid number, prompts user for a new guess.
+            $("#response").html("Please enter a whole number.").css({color: 'red'});
             return;       
+        } else if (number > 100 || number < 1) {
+            $("#response").html("Nope, how about a whole number between 1 and 100?").css({color: 'red'});
         } else if (number === target) {  // if the user guesses the target number, return statement.
-            $("#response").html("Congrats! You got it!");
+            $("#response").html("Congrats! You got it!").css({color: 'black'});
             return;
         } else {                       // if user guesses wrong, invoke difference method to determine hot or cold.
             return difference(number);
@@ -25,12 +24,44 @@ $document.ready(function() {
 
     function difference(num) {  // determines how big of difference between user guess and target number.
         var diff = Math.abs(num-target);
-        if(diff < 10) {
-            $("#response").html("Hot!");
+        if((diff <= 10) && (previousDiff === 0)) {
+            previousDiff = diff;
+            $("#response").html("Hot!").css({color: 'black'});
+            return;
+        } else if ((diff <= 10) && (previousDiff !== 0)) {
+            lastDiff(diff);
+            return;
+        } else if ((diff > 10) && (previousDiff === 0)) {
+            previousDiff = diff;
+            $("#response").html("Cold.").css({color: 'black'});
             return;
         } else {
-            $("#response").html("Cold.");
+            lastDiff(diff);
             return;
         }
     };
+
+    function lastDiff(diff) {
+        if (diff < previousDiff) {
+            previousDiff = diff;
+            $("#response").html("Getting hotter!").css({color: 'black'});
+        } else if (diff > previousDiff) {
+            previousDiff = diff;
+            $("#response").html("Getting colder!").css({color: 'black'});
+        } else {
+            $("#response").html("Different # Please!").css({color: 'red'});
+        }
+
+    };
+    
+    $('#submit').click(function() {
+        submit();
+    });
+    $('#newGame').click(function(event) {
+        event.preventDefault();
+        target = Math.floor(Math.random()*99+1);
+        $("#response").html("New Game Started!").css({color: 'black'});
+    });
+
 });  
+
